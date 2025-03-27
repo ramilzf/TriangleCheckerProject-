e5f8109b341ea623a3fae8c424e25112638fe533
ramilzf <ramil.zeynalov@tudent.wsb.edu.pl>
2025-03-27 22:53:59 +0400

    Initial commit - triangle checker with tests

---
+++ TriangleChecker.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using TriangleChecker;
+using Xunit;
+
+public class TriangleTests
+{
+    [Theory]
+    [InlineData(3, 4, 5, true)]
+    [InlineData(1, 2, 3, false)]
+    [InlineData(5, 5, 5, true)]
+    [InlineData(10, 1, 1, false)]
+    [InlineData(6, 8, 10, true)]
+    public void CanFormTriangle_TestCases(int a, int b, int c, bool expected)
+    {
+        bool result = Triangle.CanFormTriangle(a, b, c);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ TriangleChecker/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace TriangleChecker
+{
+    public class Triangle
+    {
+        public static bool CanFormTriangle(int a, int b, int c)
+        {
+            return (a + b > c) && (a + c > b) && (b + c > a);
+        }
+    }
+}
